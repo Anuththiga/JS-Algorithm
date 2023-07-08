01b8db1f7caacf07bc92b93c2618ce1595bf05c7
Tanuththiga <anuththiga25jt@gmail.com>
2023-07-08 21:19:34 +0800

    fizzbuzz

---
+++ Fizzbuzz.js
@@ -0,0 +1,45 @@
+// Given an integer n, return a string array answer (1-indexed) where:
+
+// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+// answer[i] == "Fizz" if i is divisible by 3.
+// answer[i] == "Buzz" if i is divisible by 5.
+// answer[i] == i (as a string) if none of the above conditions are true.
+ 
+
+// Example 1:
+
+// Input: n = 3
+// Output: ["1","2","Fizz"]
+// Example 2:
+
+// Input: n = 5
+// Output: ["1","2","Fizz","4","Buzz"]
+// Example 3:
+
+// Input: n = 15
+// Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+
+function fizzbuzz(n)
+{
+    let answer = [];
+    for(let i=1; i<=n; i++)
+    {
+        if(i % 3 === 0 && i % 5 === 0)
+        {
+           console.log("fizzbuzz");
+        }
+        else if(i % 3 === 0)
+        {
+           console.log("fizz");
+        }
+        else if(i % 5 === 0)
+        {
+            console.log("Buzz");
+        }
+        else {
+            console.log(i.toString());
+        }
+    }
+}
+
+fizzbuzz(15);
