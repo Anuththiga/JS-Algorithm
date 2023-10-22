fc79c329f37df37a7e0df1b7536ee265a8d3fe0f
Anuththiga <anuththiga25jt@gmail.com>
2023-10-22 22:58:26 +0800

    1021-remove outer most parentheses

---
+++ Leetcode/String/1021-RemoveOutermostParentheses.js
@@ -0,0 +1,54 @@
+/* A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.
+
+For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+A valid parentheses string s is primitive if it is nonempty, and there does not exist a way to split it into s = A + B, with A and B nonempty valid parentheses strings.
+
+Given a valid parentheses string s, consider its primitive decomposition: s = P1 + P2 + ... + Pk, where Pi are primitive valid parentheses strings.
+
+Return s after removing the outermost parentheses of every primitive string in the primitive decomposition of s.
+
+ 
+
+Example 1:
+
+Input: s = "(()())(())"
+Output: "()()()"
+Explanation: 
+The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+Example 2:
+
+Input: s = "(()())(())(()(()))"
+Output: "()()()()(())"
+Explanation: 
+The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+Example 3:
+
+Input: s = "()()"
+Output: ""
+Explanation: 
+The input string is "()()", with primitive decomposition "()" + "()".
+After removing outer parentheses of each part, this is "" + "" = "".
+ */
+var removeOuterParentheses = function(s) {
+    /*
+    steps:
+    - create result, an empty string
+    - create j variable =0
+    - iterate through s
+    - check if element is "(" then increase j by 1
+    - if j > 1 then update result with element
+    - if element is ")" then decrease j by 1
+    - finally return result
+    */
+    let result = "";
+    let j = 0;
+
+    for(el of s) {
+        if(el == '(') j++;
+        if(j > 1) result = result + el;
+        if(el == ')') j--;
+    }
+    return result;
+};
